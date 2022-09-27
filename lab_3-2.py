2f8f352126d7330374cae95ed4edd90cc8801706
SeanSalafia <p23ssalafia@fairfieldprep.org>
2022-09-27 10:55:56 -0400

    Submission for lab 3-2

---
+++ lab_3-2.py
@@ -0,0 +1,27 @@
+#Author: Sean Salafia 9/27/22
+
+# int(a)
+# Before Running Code: The letter a cannot be an integer. It is a letter. It would be a value error
+# After Running Code: It turned out to be a name error because "a" was not defined
+#------------------------------------------------------------------------------------------------------
+
+#int('a')
+# Before Running Code: Again the string 'a' cannot be an integer. It would be a value error.
+# After Running Code: It was a value error, for the reason stated
+#------------------------------------------------------------------------------------------------------
+
+#'a' + 2
+# Before Running Code: It will be a type error because the string 'a' cannot be added to the number 2
+# After Running Code: It was a type error.
+#------------------------------------------------------------------------------------------------------
+
+#import date
+# Before Running Code: The module "date" cannot be found so it is a Module Not Found Error
+# After Running Code: Correct because "No module named 'date'"
+#------------------------------------------------------------------------------------------------------
+
+print("I'm a happy camper!)
+ # Before Running Code: This will be a syntax error because 1st, there is not quote to end the print statement, and 2nd, 
+ # the ' starts a string which also isn't completed. 
+
+ # After Running Code: 
