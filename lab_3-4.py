1f38aedd8e26aeda057930c2fff50e2f6f8d8f6e
SeanSalafia <p23ssalafia@fairfieldprep.org>
2022-09-29 12:28:15 -0400

    Submission for lab 3-3 and 3-4

---
+++ lab_3-4.py
@@ -0,0 +1,43 @@
+#Author: Sean Salafia 9/29/22
+import math
+
+
+math
+
+"""
+
+1.  math.ceil(7.15)
+2.  math.floor(19.156)
+3.  math.fabs(-42)
+4.  math.pow(2, 3)
+5.  math.log10(100)
+6.  math.sqrt(9)
+7.  math.sqrt(2)
+8.  math.factorial(5)
+9.  math.gcd(111111111, 12321)
+10. math.sin(0)
+ 
+
+ 1: 7.15, 8
+ 2: 0, 19
+ 3: 42
+ 4: 8
+ 5: 1
+ 6: 3 
+ 7: 1.41421356237
+ 8: 120
+ 9: Math is hard
+ 10: 0
+"""
+a = math.ceil(7.15)
+b = math.floor(19.156)
+c = math.fabs(-42)
+d = math.pow(2, 3)
+e = math.log10(100)
+f = math.sqrt(9)
+g = math.isqrt(2)
+h = math.factorial(5)
+i = math.gcd(111111111, 12321)
+j = math.sin(0)
+
+print(a,b,c,d,e,f,g,h,i,j)
