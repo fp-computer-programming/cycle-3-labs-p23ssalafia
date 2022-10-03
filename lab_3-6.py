7ae772ba07f880c3f71d91cfbdc7c742d2499f06
SeanSalafia <p23ssalafia@fairfieldprep.org>
2022-10-03 14:30:55 -0400

    Submission for lab 3-6

---
+++ lab_3-6.py
@@ -0,0 +1,21 @@
+#Author: Sean Salafia 10/3/22
+
+from multiprocessing.pool import ApplyResult
+
+
+statement_1 = 'Fairfield'
+statement_2 = 'Prep'
+
+print (statement_1 + ' ' + statement_2)
+
+
+dashed_line = '-'
+print (dashed_line * 20)
+
+book_recommendation = 'I\'m loving this short story I\'m reading, "The Fall of the House of Usher"'
+print (book_recommendation)
+
+print (len(book_recommendation))
+
+fruit = 'apple'
+print (fruit[3])
